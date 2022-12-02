9e116c76de2f4b5c3a87b7f6714dc88bee67aacd
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-12-02 21:41:32 +0530

    Add project files.

---
+++ nested/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the 3 number ");
+            int n1=Convert.ToInt32(Console.ReadLine()); 
+            int n2=Convert.ToInt32(Console.ReadLine());
+            int n3=Convert.ToInt32(Console.ReadLine());
+            if(n1>n2)
+            {
+                if (n1 > n3)
+                {
+                    Console.WriteLine($"{n1} is greatest number");
+                }
+                else
+                {
+                    Console.WriteLine($"{n3} is greatest number");
+                }
+            }
+            else
+            {
+                if(n2>n3)
+                {
+                    Console.WriteLine($"{n2} is greatest number");
+                }
+                else
+                {
+                    Console.WriteLine($"{n3} is greatest number");
+                }
+            }
+        }
+    }
+}
